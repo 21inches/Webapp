ff8e9f96db3934df600099c648d3e7a6e90bcfec
Sam <meisamtaher71@gmail.com>
2025-07-29 21:16:42 +0330

    change navbar and add connect wallet rainbow kit

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { PropsWithChildren } from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+interface OwnProps {}
+
+type Props = PropsWithChildren<OwnProps>;
+
+export function Navbar(props: Props) {
+  return (
+    <header className="sticky top-0 left-0 bg-gradient-to-r p-6 from-indigo-200 via-purple-200 to-pink-200 w-full">
+      <div className="flex">
+        <Image src="./next.svg" alt="21 Inches" width={100} height={50} />
+        <div className="flex-1" />
+        <ConnectButton></ConnectButton>
+      </div>
+    </header>
+  );
+}

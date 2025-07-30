ddb1a6f81d76b2ad41c3347ce8a2df6c453f2876
Parsa Hosseini <officer47p@gmail.com>
2025-07-30 15:23:50 +0330

    Add 21inches logo

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -3,11 +3,18 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export function Navbar() {
   return (
-    <header className="sticky top-0 left-0 bg-gradient-to-r p-6 from-indigo-200 via-purple-200 to-pink-200 w-full">
+    <header className="sticky top-0 left-0 bg-white w-full">
       <div className="flex">
-        <Image src="./next.svg" alt="21 Inches" width={100} height={50} />
+        <Image
+          src="/21incheslogo.png"
+          alt="21 Inches"
+          width={100}
+          height={100}
+        />
         <div className="flex-1" />
-        <ConnectButton></ConnectButton>
+        <div className="flex items-center justify-end pr-4">
+          <ConnectButton></ConnectButton>
+        </div>
       </div>
     </header>
   );
